7edd2642d282d6f77509d95e37227aa68ab3e685
Antonio Harger Rivero <antonio.harger@gmail.com>
2018-07-21 20:07:55 -0300

    Scene Change

---
+++ VanderJames/Assets/VDJ/BuilderGame/Movement/NoMovement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace VDJ.BuilderGame.Movement
+{
+    public class NoMovement : IMovement
+    {
+        private Rigidbody rb;
+
+        public NoMovement(Rigidbody rb)
+        {
+            this.rb = rb;
+        }
+
+        public void Begin()
+        {
+            rb.velocity = Vector3.zero;
+        }
+
+        public void Leave()
+        {
+            
+        }
+
+        public void MoveFixedUpdate()
+        {
+            rb.velocity = Vector3.zero;
+        }
+
+        public void MoveLateUpdate()
+        {
+            
+        }
+
+        public void MoveUpdate()
+        {
+            
+        }
+    }
+}
---
+++ VanderJames/Assets/VDJ/BuilderGame/UI/GameUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour {
+    public static GameUI Instance { get; private set; }
+
+
+    public Text text;
+
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void DisableCountdown()
+    {
+        text.gameObject.SetActive(false);
+    }
+
+    public void EnableCountdown()
+    {
+        text.gameObject.SetActive(true);
+    }
+    public void SetCountdown(int val)
+    {
+        text.text = val.ToString();
+    }
+}
