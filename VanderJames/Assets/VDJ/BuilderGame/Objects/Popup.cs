3ffedecaabea6b47a733d5c979fd6658987b7673
Antonio Harger Rivero <antonio.harger@gmail.com>
2018-07-21 07:58:58 -0300

    Popup Tweening

--- VanderJames/Assets/VDJ/BuilderGame/Objects/Popup.cs
+++ VanderJames/Assets/VDJ/BuilderGame/Objects/Popup.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NaughtyAttributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +9,40 @@ namespace VDJ.BuilderGame.Objects
 {
     public class Popup : MonoBehaviour
     {
+        [Serializable]
+        public class TransformData
+        {
+            public Vector3 Position;
+            public Quaternion Rotation;
+            public Vector3 Scale;
+        }
+
+        [HideInInspector]
+        public TransformData start;
+        [HideInInspector]
+        public TransformData end;
+
         [Range(0, 1)]
         public float progress;
 
 
-
+        
 
 
         public void Update()
         {
-            
+            transform.Apply(PopupHelpers.Lerp(start, end, progress));
+        }
+
+        [Button]
+        public void SaveStart()
+        {
+            start = PopupHelpers.SaveFrom(transform);
+        }
+        [Button]
+        public void SaveEnd()
+        {
+            end = PopupHelpers.SaveFrom(transform);
         }
 
     }
---
+++ VanderJames/Assets/VDJ/BuilderGame/Objects/PopupHelpers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace VDJ.BuilderGame.Objects
+{
+    public static class PopupHelpers
+    {
+        public static void Apply(this Transform transform, Popup.TransformData data)
+        {
+            transform.localPosition = data.Position;
+            transform.localRotation = data.Rotation;
+            transform.localScale = data.Scale;
+        }
+
+        public static Popup.TransformData Lerp(Popup.TransformData d1, Popup.TransformData d2, float t)
+        {
+            return new Popup.TransformData()
+            {
+                Position = Vector3.Lerp(d1.Position, d2.Position, t),
+                Rotation = Quaternion.Lerp(d1.Rotation, d2.Rotation, t),
+                Scale = Vector3.Lerp(d1.Scale, d2.Scale, t)
+            };
+        }
+
+        public static Popup.TransformData SaveFrom(Transform transform)
+        {
+            return new Popup.TransformData()
+            {
+                Position = transform.localPosition,
+                Rotation = transform.localRotation,
+                Scale = transform.localScale
+            };
+        }
+    }
+}
+
