acb82dbe246485a661e4c6871c3cd26e1bbba591
Antonio Harger Rivero <antonio.harger@gmail.com>
2018-07-21 00:25:19 -0300

    Basic Player Movement

---
+++ VanderJames/Assets/VDJ/BuilderGame/Objects/Popup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace VDJ.BuilderGame.Objects
+{
+    public class Popup : MonoBehaviour
+    {
+        [Range(0, 1)]
+        public float progress;
+
+
+
+        public void Update()
+        {
+            
+        }
+
+    }
+}
---
+++ VanderJames/Assets/VDJ/BuilderGame/PlayerInput.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace VDJ.BuilderGame
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        public KeyCode mainKey = KeyCode.Space;
+
+
+        public float Horizontal
+        {
+            get
+            {
+                return Input.GetAxisRaw("Horizontal");
+            }
+        }
+
+        public float Vertical
+        {
+            get
+            {
+                return Input.GetAxisRaw("Vertical");
+            }
+        }
+
+        public bool IsMainButtonPressed
+        {
+            get
+            {
+                return Input.GetKeyDown(mainKey);
+            }
+        }
+
+        public Vector3 MoveVector { get { return new Vector3(Horizontal, 0, Vertical).normalized; } }
+    }
+}
