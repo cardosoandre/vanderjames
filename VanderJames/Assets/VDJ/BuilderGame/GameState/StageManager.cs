823922adb7e3392eb73c662e5be1ce9e23529e08
Antonio Harger Rivero <antonio.harger@gmail.com>
2018-07-22 01:29:22 -0300

    Resource spawn and building activate Systems

--- VanderJames/Assets/VDJ/BuilderGame/GameState/StageManager.cs
+++ VanderJames/Assets/VDJ/BuilderGame/GameState/StageManager.cs
@@ -1,18 +1,42 @@
-﻿using System;
+﻿using NaughtyAttributes;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
 using VDJ.BuilderGame.GameState;
+using VDJ.BuilderGame.Objects.Buildings;
 
 namespace VDJ.BuilderGame.GameState
 {
     public class StageManager : MonoBehaviour
     {
         public static StageManager Instance { get; private set; }
+        [Serializable]
+        public class Settings
+        {
+            public int startingBuildings = 3;
+            public float buildingActivationWait = 10;
+            public float totalTime = 120;
+
+            public int startingResourceSpawn = 2;
+            public int resourceSpawnsPerTime = 1;
+            public float resourceSpawnWait = 3.0f;
+            public float spawnRange = .5f;
+
+
+            public GameObject stonePrefab;
+            public GameObject woodPrefab;
+        }
+
+        public Settings settings;
+        [ReorderableList]
+        public List<BuildingSpot> buildingSpots;
 
         
+        public List<Transform> spawningSpots;
+
 
         private void Awake()
         {
@@ -38,8 +62,70 @@ namespace VDJ.BuilderGame.GameState
 
             Debug.Log("Activating Players");
             PlayerManager.Instance.ActivatePlayers();
+
+
+            StartCoroutine(ActiavtePeriodicalBuildings());
+            StartCoroutine(SpawnPeriodicalResources());
+            
         }
 
+
+        private IEnumerator SpawnPeriodicalResources()
+        {
+            SpawnResources(settings.startingResourceSpawn);
+
+            while (buildingSpots.Count > 0)
+            {
+                yield return new WaitForSeconds(settings.resourceSpawnWait);
+                SpawnResources(settings.resourceSpawnsPerTime);
+            }
+        }
+
+        private void SpawnResources(int spawnsPerType)
+        {
+            var randomSort = spawningSpots.OrderBy(x => UnityEngine.Random.value).Take(spawnsPerType * 2).ToArray();
+
+            for (int i = 0; i < randomSort.Length; i++)
+            {
+
+                var spawnPoint = randomSort[i].transform.position + UnityEngine.Random.insideUnitSphere * settings.spawnRange;
+                if (i%2 == 0)
+                {
+                    Spawn(settings.stonePrefab, spawnPoint);
+                }else
+                {
+                    Spawn(settings.woodPrefab, spawnPoint);
+                }
+            }
+
+        }
+
+        private void Spawn(GameObject prefab, Vector3 spawnPoint)
+        {
+            Instantiate(prefab, spawnPoint, prefab.transform.rotation);
+        }
+
+        private IEnumerator ActiavtePeriodicalBuildings()
+        {
+
+            for (int i = 0; i < settings.startingBuildings; i++)
+            {
+                ActivateNextBuilding();
+            }
+
+            while(buildingSpots.Count > 0)
+            {
+                yield return new WaitForSeconds(settings.buildingActivationWait);
+                ActivateNextBuilding();
+            }
+        }
+
+        private void ActivateNextBuilding()
+        {
+            var spot = buildingSpots[0];
+            buildingSpots.RemoveAt(0);
+            spot.Activate();
+        }
 
         private IEnumerator GameStartCoroutine()
         {
@@ -53,6 +139,14 @@ namespace VDJ.BuilderGame.GameState
             GameUI.Instance.SetCountdown(1);
             yield return new WaitForSeconds(1.0f);
             GameUI.Instance.DisableCountdown();
+        }
+
+
+        [Button]
+        private void LoadAllBuildings()
+        {
+            buildingSpots = new List<BuildingSpot>(FindObjectsOfType<BuildingSpot>());
+
         }
     }
 }
---
+++ VanderJames/Assets/VDJ/BuilderGame/Objects/Buildings/BuildingSpot.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Events;
+using VDJ.BuilderGame.Resources;
+
+namespace VDJ.BuilderGame.Objects.Buildings
+{
+    public class BuildingSpot : MonoBehaviour
+    {
+        [Serializable]
+        public class Settings
+        {
+            public ResourceCost cost;
+            public Sprite sprite;
+            public string name;
+            public int score;
+        }
+
+
+        public SpriteRenderer sr;
+        public ResourceDeliveryPoint deliveryPoint;
+
+        public Transform hiddenPortion;
+
+        public UnityEvent Activated;
+
+
+        public void Init(Settings settings)
+        {
+            sr.sprite = settings.sprite;
+            deliveryPoint.cost = settings.cost;
+
+        }
+
+        public void Activate()
+        {
+            hiddenPortion.gameObject.SetActive(true);
+            Activated.Invoke();
+        }
+    }
+}
